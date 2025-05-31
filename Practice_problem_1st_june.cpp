bf6f823970b143b0ca338cc92a1f239b344fac40
KRISH BHARDWAJ <krishbhardwaj@KRISHs-MacBook-Air.local>
2025-05-31 21:37:04 +0530

    Practice problem june 1

---
+++ Practice_problem_1st_june.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+   
+    int digit ;
+    cout <<" enter digit number ";
+    cin>>digit ;
+    if ( digit ==1)
+    {
+        cout <<" one "<<endl;
+    }
+    else if ( digit ==2)
+    {
+        cout <<" two"<<endl;
+    }
+    else if ( digit ==3)
+    {
+        cout <<" three"<<endl;
+    }
+    else if ( digit ==4)
+    {
+        cout <<" four"<<endl;
+    }
+    else if ( digit ==5)
+    {
+        cout <<" five"<<endl;
+    }
+    else if ( digit ==6)
+    {
+        cout <<" six"<<endl;
+    }
+    else if ( digit ==7)
+    {
+        cout <<" seven"<<endl;
+    }
+    else if ( digit ==8)
+    {
+        cout <<" eight"<<endl;
+    }
+    else if ( digit ==9)
+    {
+        cout <<" nine"<<endl;
+    }
+    else if ( digit ==0)
+    {
+        cout <<" zero"<<endl;
+    }
+    else
+    {
+        cout <<" invalid";
+    }
+    
+    
+    return 0;
+}
+    
+    
