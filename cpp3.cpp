52db13d39f304152d9ff7638bf683fbf813591d7
bkrish17 <krishbharadwaj.07@gamil.com>
2025-07-07 12:09:44 +0530

    largest element of array

---
+++ cpp3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[7]={4,8,6,9,5,2,7};
+    int i,n=7,max;
+    max=A[0];
+    for (i=1;i<7;i++)
+    {
+        if (A[i]>max)
+        {
+            max =A[i];
+        }
+    }
+    cout<<" maximum no. is "<<max;
+    return 0;
+    
+}
