5cc8c5556495cbc0324ab03bf31cb5eb1d63589c
bkrish17 <krishbharadwaj07@gmail.com>
2025-07-22 09:40:56 +0530

    sum of two number

---
+++ cpp12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+   
+    int a,b,c;
+    cout <<" enter two no";
+    cin >>a>>b;
+    c=a+b;
+    cout<<" sum of two no."<<c;
+    
+    return 0;
+}
+
